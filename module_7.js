5f7d93f7c0bc3f705d70996ad43129b8a46e3dcd
Maksym-Maleiev <maximmaleev93@gmail.com>
2021-12-06 21:31:05 +0200

    add inf

---
+++ module_7.js
@@ -0,0 +1,98 @@
+// =========Всплытие_событий============
+
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+
+// parent.addEventListener("click", () => {
+//   alert("Parent click handler");
+// });
+
+// child.addEventListener("click", () => {
+//   alert("Child click handler");
+// });
+
+// descendant.addEventListener("click", () => {
+//   alert("Descendant click handler");
+// });
+
+
+// =========Свойство_event.target=========
+
+
+// const parent = document.querySelector("#parent");
+
+// parent.addEventListener("click", (event) => {
+//   console.log("event.target: ", event.target);
+//   console.log("event.currentTarget: ", event.currentTarget);
+// });
+
+// =========Прекращение_всплытия#============
+
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+
+// parent.addEventListener("click", () => {
+//   alert(
+//     "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
+
+// child.addEventListener("click", () => {
+//   alert(
+//     "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
+
+// descendant.addEventListener("click", (event) => {
+//   event.stopPropagation();
+//   alert("Descendant click handler");
+// });
+
+// =============Делегирование_событий===========
+
+// ==========Палитра_цветов================
+
+const colorPalette = document.querySelector(".color-palette");
+const output = document.querySelector(".output");
+
+colorPalette.addEventListener("click", selectColor);
+
+// This is where delegation «magic» happens
+function selectColor(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  const selectedColor = event.target.dataset.color;
+  output.textContent = `Selected color: ${selectedColor}`;
+  output.style.color = selectedColor;
+}
+
+// Some helper functions to render palette items
+createPaletteItems();
+
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < 60; i++) {
+    const color = getRangomColor();
+    const item = document.createElement("button");
+    item.type = "button";
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.classList.add("item");
+    items.push(item);
+  }
+  colorPalette.append(...items);
+}
+
+function getRangomColor() {
+  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+}
+
+function getRandomHex() {
+  return Math.round(Math.random() * 256)
+    .toString(16)
+    .padStart(2, "0");
+}
